798fa6fae693cf565952969ee390fcfddc90fe34
정성헌 <huv3838@gmail.com>
2025-04-11 17:26:54 +0900

    대쉬 정상화

--- CProcessing/Processing_Empty/unit.h
+++ CProcessing/Processing_Empty/unit.h
@@ -1,13 +1,33 @@
 #pragma once
-
-
 typedef struct PlayerCharacter
 {
     CP_Vector pos;
     float spd;
+	int isDashing;
+	float dashTime;
+	float dashTimer;
+	float dashSpeedBoost;
+	float originalSpd;
+	float dashDecayRate;
+	float size;
+	CP_Vector direction;
 } PlayerCharacter;
+extern PlayerCharacter player;
+#pragma once
+typedef struct Enemy
+{
+	CP_Vector pos;
+	float spd;
+	int isAttack;
+	float appTime;
+	float size;
+} Enemy;
+extern Enemy enemy;
 
 void PlayerMove(void);
 void DrawPlayer(float x, float y);
 void InitChar(float, float);
 void ChangeSpeed(void);
+void Dash(void);
+void PlayerInit(void);
+void DrawWall(void);
