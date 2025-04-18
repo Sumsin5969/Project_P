0d673fea62814b8eb1f9afff6fbf19a4387e79c9
정성헌 <huv3838@gmail.com>
2025-04-18 16:42:14 +0900

    [정성헌] ReadMe.md Update

--- CProcessing/Processing_Empty/StageManager.c
+++ CProcessing/Processing_Empty/StageManager.c
@@ -1,6 +1,20 @@
 #include "StageManager.h"
+#include "MyC/ZoomCamera.h"
 
-void a()
+void PreviousStage()
+{
+	//zoomin
+	ZoomIn();
+	//적 캐릭터 및 공격 일시정지
+	// 
+	//캐릭터의 포지션을 카메라의 포지션으로 초기화
+	
+	//몇 초 후 재생
+
+}
+
+void NextStage()
 {
 
 }
+//
